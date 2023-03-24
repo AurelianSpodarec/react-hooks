677099cdaf6da0747fe9173a7316105883debdae
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-24 11:03:45 +0000

    refactor: Refactored the default app structure and added Sass

---
+++ src/views/pages/Layout.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+
+function Layout() {
+    return (
+        <BrowserRouter>
+
+                <main className="bg-[#071520]">
+                    Test
+                </main>
+
+        </BrowserRouter>
+    )
+}
+
+export default Layout;
